c56260c7e14e563678db8857f6899a6f65ca2075
Michał Nowakowski <nowakowskimichal1988@gmail.com>
2019-10-25 11:28:44 +0200

    Finished

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  'GOOGLE_CLIENT_ID': '165512686209-4f67ec6v1sepps1g203rvlgutrnftk50.apps.googleusercontent.com',
+  'GOOGLE_CLIENT_SECRET': 'nq-wuvn5FWaoEGvzMl5gsO2q',
+  'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
